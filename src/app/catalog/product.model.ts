88885ab25c628e62ae0fd05d90922b6902b55579
Aroita <aroitaim84@gmail.com>
2024-05-06 17:54:00 +0200

    update ngClass

---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    description: string;
+    name: string;
+    imageName: String;
+    category: string;
+    price: number;
+    discount: number;
+}
